ca3e913cbf748a0891ee12a6044bae96ea325bd8
Abraham Milgram <58184483+abraham-milgram@users.noreply.github.com>
2022-05-25 14:37:46 -0400

    pusha t(o github)

--- New/polynomial.py
+++ New/polynomial.py
@@ -1,10 +1,14 @@
+from operator import itemgetter
 from term import term
 
 
 class Polynomial:
-    def __init__(self, terms): # Terms is [coef, {variable1: degree, variable2: degree}
-        self.terms = [term(t) for t in terms]
-        self.degree = sum([t.degree for t in self.terms])
+    def __init__(self, terms_input): # Terms is [coef, {variable1: degree, variable2: degree}
+        self.terms = [term(t) for t in terms_input]
+        self.degree = max([t.degree for t in self.terms])
 
+    def sort(self):
+        self.terms = sorted(self.terms, key=lambda self: self.terms.degree)
 poly = Polynomial([[3, {'x': 2}], [4, {'x': 1}], [-5, {}]])
+
 print(poly.degree)
