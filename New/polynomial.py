8b8c1a5c6bb28d1e9d2316939db3a8008da8a42b
Abraham Milgram <58184483+abraham-milgram@users.noreply.github.com>
2022-05-25 21:45:56 -0400

    hollllllllyyyyyy debug/ divide func

--- New/polynomial.py
+++ New/polynomial.py
@@ -1,5 +1,9 @@
+from copy import deepcopy
 from term import term
-
+"""
+Issues:
+* When the vars of a term are changed it's degree is not updated
+"""
 def simplify(terms):
     simpler = {} # {vars as derived from t (term): [sum of coefs, [term objects]]}
     for t in terms:
@@ -28,14 +32,16 @@ def add(p1, p2):
     return Polynomial(simplify(p1.terms + p2.terms))
 
 def sub(p1, p2):
-    return Polynomial(simplify(p1.terms + mult(p2, -1).terms))
+    negative = mult(p2, -1)
+    equation = p1.terms + negative.terms
+    return Polynomial(simplify(equation))
 
 def mult(p1, p2):
     terms = []
     if not isinstance(p2, Polynomial):
         p2 = Polynomial([[p2,{}]])
     if not isinstance(p1, Polynomial):
-        p2 = Polynomial([[p1,{}]])
+        p1 = Polynomial([[p1,{}]])
     for n in p1.terms:
         for m in p2.terms:
             coef = n.coef * m.coef
@@ -55,26 +61,33 @@ class Polynomial:
     def __init__(self, terms_input):
         # term object is composed of operator, coef, vars, degree
         self.terms = [term(t) for t in terms_input] if type(terms_input[0]) == list else terms_input
-        self.simplify()
         self.sort()
         
         self.degree = self.terms[0].degree
 
     def simplify(self):
         self.terms = simplify(self.terms)
-    def print(self):
+    def print(self, return_=False):
         self.sort()
         outpolys = []
         for t in self.terms: # prettifying terms
-            polystr = f'{t.operator} {abs(t.coef)}'
+            polystr = f'{t.operator} {abs(t.coef) if t.coef != 1 else ""}'
             for k, v in t.vars.items():
-                polystr += f"{k}^{v}" if v != 1 else f"{k}"
+                if v not in [1, 0]: 
+                    polystr += f"{k}^{v}"
+                elif v == 0:
+                    polystr += "" 
+                else:
+                    polystr += f"{k}"
             outpolys.append(polystr)
         
         if '+' in outpolys[0]: # delete leading '+'
             outpolys[0] = outpolys[0].replace('+ ', '') 
         
-        print(' '.join(outpolys))
+        if return_:
+            return ' '.join(outpolys) # if return_ arg is true, returned instead of printed
+
+        print(' '.join(outpolys)) 
 
     def sort(self): # might want to tweak this/degree of terms
         self.terms = sorted(self.terms, key=lambda self: self.degree, reverse=True)
@@ -85,7 +98,8 @@ multip = mult(poly2, poly)
 multip.print()
 #divid/divis"""
 
-def single_div(dividend, divisor): #This is their leading terms
+def single_div(dividend1, divisor): #This is their leading terms
+    dividend = deepcopy(dividend1)
     # Divide coefs 
     coef = dividend.coef/divisor.coef
     # subtract exponents
@@ -100,25 +114,37 @@ def single_div(dividend, divisor): #This is their leading terms
 # a = term([10, {'x': }])
 # b = term([1, {'x': 2}])
 # Polynomial([single_div(a,b)]).print()
-def div(dividend1, divisor1):
+def div(dividend1, divisor1, printed=False):
     dividend = dividend1.terms
     divisor = divisor1.terms
     answer = []
-
+    break_ = False      
     while 1:
+        dividend = [term for term in dividend if term.coef != 0]
         # divide the leading term of the active dividend by the leading term of the divisor
+        leading_div = single_div(dividend[0], divisor[0])
+        # if we divide the leading term of the coefficient and the leading term of the divisor, if there is a negetive exponenet then the dividend is the remainder
+        for power in list(leading_div.vars.values()):
+            if not power + 1: # Handle zeros
+                remainder = dividend
+                break_ = True
+        if break_:
+            break
         # recording that result to the answer
-        answer.append(single_div(dividend[0], divisor[0]))
+        answer.append(leading_div)
         # Multiply -1th term in the answer by the divisor
         result = mult(Polynomial([answer[-1]]), divisor1)
         # subtract the dividend by the result
         dividend = sub(Polynomial(dividend), result).terms
-        # if we divide the leading term of the coefficient and the leading term of the divisor, if there is a negetive exponenet then the dividend is the remainder
-        #3x^4y^5/x^5
+    
+    answer = [Polynomial(answer), [Polynomial(remainder), divisor1]]
+    if printed: # If print argument is specified to be true than a formated answer is returned
+        answer = f"{answer[0].print(True)} + ({answer[1][0].print(True)})/({answer[1][1].print(True)})"
+    return answer
 
         
-a = Polynomial([[2, {'x':3}], [4, {'x': 2}], [5, {'x': 1}], [-1, {''}]])
+a = Polynomial([[2, {'x':3}], [4, {'x': 2}], [5, {'x': 1}], [-1, {}]])
 
-b = Polynomial([[[1, {'x': 2}], [3, {'x': 1}]]])
+b = Polynomial([[1, {'x': 2}], [-3, {'x': 1}]])
 
-div(a, b)
+print(div(a, b, True))
